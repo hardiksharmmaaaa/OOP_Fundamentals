5bf1c008631fc0617ff7381fbafdaed4786afbd0
Hardik Sharma <hardiksharmaa007@gmail.com>
2025-03-15 17:21:12 +0530

    added first commits

---
+++ class.py
@@ -0,0 +1,9 @@
+# creating the class 
+
+class Student: 
+    name="Hardik Sharma"
+
+# Creating the object for the particular Class 
+
+obj=Student()
+print(obj.name)
---
+++ constructor.py
@@ -0,0 +1,11 @@
+class student():
+    def __init__(self,Name):
+        self.name=Name
+
+s1=student("Hardik Sharma")
+s2=student("Archita Jain")
+s3=student("Sahil Gupta")        
+
+print(s1.name)
+print(s2.name)
+print(s3.name)
