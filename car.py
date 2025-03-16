296d7c284274d07df52fedfc73596187c384e835
Hardik Sharma <hardiksharmaa007@gmail.com>
2025-03-16 09:37:16 +0530

    added the Car.py

---
+++ car.py
@@ -0,0 +1,15 @@
+## adding a class car for the required specs of the car 
+
+class car:
+    def __init__(self):
+        pass
+    Brand="Maruti"
+    Color=input("Enter the COlor on the basis of the requirements")
+    qty=input("Enter the Quantity ")
+
+## adding the objects (models of the car) 
+
+car1=car()
+print(car.Color)
+print(car.qty)
+print(car.Brand)
